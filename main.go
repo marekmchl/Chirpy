ab311211f3ebba24271fa4f8da359bd77716a9ac
marekmchl <marekmchl@proton.me>
2025-05-28 11:50:09 +0200

    Update handler for getting metrics, separate out admin endpoints

--- main.go
+++ main.go
@@ -18,8 +18,14 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 }
 
 func (cfg *apiConfig) handlerGetMetrics(w http.ResponseWriter, r *http.Request) {
-	message := fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load())
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	messageHTML := `<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+  </body>
+</html>`
+	message := fmt.Sprintf(messageHTML, cfg.fileserverHits.Load())
+	w.Header().Add("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(200)
 	w.Write([]byte(message))
 }
@@ -41,8 +47,8 @@ func main() {
 		w.WriteHeader(200)
 		w.Write([]byte{'O', 'K'})
 	})
-	serveMux.HandleFunc("GET /api/metrics", cfg.handlerGetMetrics)
-	serveMux.HandleFunc("POST /api/reset", cfg.handlerResetMetrics)
+	serveMux.HandleFunc("GET /admin/metrics", cfg.handlerGetMetrics)
+	serveMux.HandleFunc("POST /admin/reset", cfg.handlerResetMetrics)
 	server := http.Server{
 		Addr:    ":8080",
 		Handler: &serveMux,
