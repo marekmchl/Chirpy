f71f1ff7a610713639bd4f59a8f1aef1a5d08420
marekmchl <marekmchl@proton.me>
2025-05-28 09:41:55 +0200

    Made into a basic file server

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import "net/http"
 
 func main() {
-	fmt.Println("Hello, world!")
+	serveMux := http.ServeMux{}
+	serveMux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	serveMux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		w.Write([]byte{'O', 'K'})
+	})
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: &serveMux,
+	}
+	server.ListenAndServe()
 }
